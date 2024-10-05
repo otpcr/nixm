6e88683a8ee73bd02f04e24c58d953db2fc65646
Bart Thate <rssbotd@gmail.com>
2024-10-05 14:17:00 +0200

    remove req

--- nixm/modules/face.py
+++ nixm/modules/face.py
@@ -6,7 +6,7 @@
 "all modules"
 
 
-from . import cmd, err, fnd, irc, log, mod, req, rss, tdo, thr, upt
+from . import cmd, err, fnd, irc, log, mod, rss, tdo, thr, upt
 
 
 def __dir__():
@@ -17,7 +17,6 @@ def __dir__():
         'irc',
         'log',
         'mod',
-        'req',
         'rss',
         'tdo',
         'thr',
