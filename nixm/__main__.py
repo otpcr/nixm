986953068aa8a9a8016821cb404a1b980a366dea
Bart Thate <rssbotd@gmail.com>
2024-11-01 15:01:26 +0100

    add main

---
+++ nixm/__main__.py
@@ -0,0 +1,106 @@
+# This file is placed in the Public Domain.
+# pylint: disable=C,W0105,W0611,W0718
+
+
+"console"
+
+
+import os
+import readline
+import sys
+import termios
+import time
+
+
+from nixt.object  import Config
+from nixt.persist import Workdir
+from nixt.runtime import Client, Errors, Event, errors, later
+
+
+from .command import NAME, command, forever, parse, scanner
+from .        import face
+
+
+"defines"
+
+
+cfg  = Config()
+
+
+"classes"
+
+
+class Console(Client):
+
+    def __init__(self):
+        Client.__init__(self)
+        self.register("command", command)
+
+    def callback(self, evt):
+        Client.callback(self, evt)
+        evt.wait()
+
+    def poll(self):
+        evt = Event()
+        evt.txt = input("> ")
+        evt.type = "command"
+        return evt
+
+    def raw(self, txt):
+        print(txt)
+
+
+"utilities"
+
+
+def banner():
+    tme = time.ctime(time.time()).replace("  ", " ")
+    print(f"{NAME.upper()} since {tme}")
+
+
+def wrap(func):
+    old = None
+    try:
+        old = termios.tcgetattr(sys.stdin.fileno())
+    except termios.error:
+        pass
+    try:
+        func()
+    except (KeyboardInterrupt, EOFError):
+        print("")
+    except Exception as ex:
+        later(ex)
+    finally:
+        if old:
+            termios.tcsetattr(sys.stdin.fileno(), termios.TCSADRAIN, old)
+    for txt in errors():
+        print(txt)
+
+
+"runtime"
+
+
+def main():
+    parse(cfg, " ".join(sys.argv[1:]))
+    if "v" in cfg.opts:
+        banner()
+    for mod, thr in scanner(face, init="i" in cfg.opts, disable=cfg.sets.dis):
+        if "v" in cfg.opts and "output" in dir(mod):
+            mod.output = print
+        if thr and "w" in cfg.opts:
+            thr.join()
+    if "c" in cfg.opts:
+        csl = Console()
+        csl.start()
+        forever()
+    elif cfg.txt:
+        evt = Event()
+        evt.txt = cfg.txt
+        evt.type = "command"
+        csl = Console()
+        command(csl, evt)
+        evt.wait()
+    
+
+if __name__ == "__main__":
+    wrap(main)
